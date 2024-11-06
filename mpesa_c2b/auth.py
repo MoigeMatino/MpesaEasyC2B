8a9740c201e5c874f538f10ccc667b924dc4db43
AgathaBahati <matinodev@gmail.com>
2024-11-06 18:23:14 +0300

    fix: fix successful token retrieval test

--- mpesa_c2b/auth.py
+++ mpesa_c2b/auth.py
@@ -16,6 +16,6 @@ class MpesaAuth:
         token_url = f"{self.base_url}/oauth/v1/generate?grant_type=client_credentials"
         response = requests.get(token_url, auth=HTTPBasicAuth(self.consumer_key, self.consumer_secret))
         if response.status_code == 200:
-            response.json().get('access_token')
+            return response.json().get('access_token')
         else:
             raise Exception("Failed to obtain access token: " + response.text)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,14 +1,13 @@
 import pytest
 from unittest.mock import patch, Mock
 import requests
-#import MpesaAuth class from auth module
+from mpesa_c2b.auth import MpesaAuth
 
 @pytest.fixture
 def mpesa_auth():
     consumer_secret = "test_consumer_secret"
     consumer_key = "test_consumer_key"
-    # return instance of MPesaAuth class
-    # return MpesaAuth(consumer_secret, consumer_key)
+    return MpesaAuth(consumer_secret, consumer_key)
     
 
 @patch('mpesa_c2b.auth.requests.get')
@@ -19,6 +18,7 @@ def test_successful_token_retrieval(mock_get, mpesa_auth):
     mock_get.return_value = mock_response
 
     token = mpesa_auth.get_access_token()
+    
     assert token == 'test_token'
     
 
@@ -26,11 +26,12 @@ def test_successful_token_retrieval(mock_get, mpesa_auth):
 def test_invalid_credentials(mock_get, mpesa_auth):
     mock_response = Mock()
     mock_response.status_code = 401
-    mock_response.json.return_value = {'errorCode': 'test_error_code', 'errorMessage': 'test_error_message'}
+    mock_response.json.return_value = {'error_code': 'test_error_code', 'error_message': 'test_error_message'}
     mock_get.return_value = mock_response
     
     with pytest.raises(Exception) as e:
         mpesa_auth.get_access_token()
+    import pdb; pdb.set_trace()
     assert str(e.value) == "test_error_message"
     
 @patch('mpesa_c2b.auth.requests.get')
@@ -40,7 +41,7 @@ def test_network_error_handling(mock_get, mpesa_auth):
 
     with pytest.raises(Exception) as e:
         mpesa_auth.get_access_token()
-    assert str(e.value) == "Network error" 
+    assert "Network error" in str(e.value)
     
 
 
